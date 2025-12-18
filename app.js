8b6ac53e3a9611b31504f62ce482af9649c8e4f5
tim <tim@smashedcrabstudio.com>
2025-12-18 01:59:57 +0000

    update test site

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getDatabase, ref, onValue, runTransaction } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 /* -----------------------------
-   CONFIG
---------------------------------*/
-const ITEM_COUNT = 28;
-
-/* -----------------------------
    FIREBASE CONFIG
 --------------------------------*/
 const firebaseConfig = {
@@ -21,6 +16,41 @@ const firebaseConfig = {
 };
 
 /* -----------------------------
+   CONFIG
+--------------------------------*/
+
+const inventoryItems = [
+  { image: "inv_1.png", title: "Orange Salamander" },
+  { image: "inv_2.png", title: "Agility arena ticket" },
+  { image: "inv_3.png", title: "Monkey Nuts" },
+  { image: "inv_4.png", title: "Opal Bolts" },
+  { image: "inv_5.png", title: "Lit Sapphire Lantern" },
+  { image: "inv_6.png", title: "Tribal Mask (any)" },
+  { image: "inv_7.png", title: "Nettle Tea" },
+  { image: "inv_8.png", title: "Vampyre Dust" },
+  { image: "inv_9.png", title: "Wild Pie" },
+  { image: "inv_10.png", title: "Snake Weed" },
+  { image: "inv_11.png", title: "Super Antipoison (3)" },
+  { image: "inv_12.png", title: "Cave Nightshade" },
+  { image: "inv_13.png", title: "Poisoned Karambwan" },
+  { image: "inv_14.png", title: "Silver Sickle" },
+  { image: "inv_15.png", title: "Mud Rune" },
+  { image: "inv_16.png", title: "Adamant Halberd" },
+  { image: "inv_17.png", title: "Eclipse Red" },
+  { image: "inv_18.png", title: "Cod" },
+  { image: "inv_19.png", title: "Amulet of Magic" },
+  { image: "inv_20.png", title: "Snelm (any)" },
+  { image: "inv_21.png", title: "Kebbit Bolts" },
+  { image: "inv_22.png", title: "Pheonix Feather" },
+  { image: "inv_23.png", title: "Mithril Dart" },
+  { image: "inv_24.png", title: "Limpwurt Root" },
+  { image: "inv_25.png", title: "Volcanic Sulphur" },
+  { image: "inv_26.png", title: "Sunfire Splinters" },
+  { image: "inv_27.png", title: "Lizardman Fang" },
+  { image: "inv_28.png", title: "Blessed Wyrm Bones" }
+];
+
+/* -----------------------------
    INIT FIREBASE
 --------------------------------*/
 const app = initializeApp(firebaseConfig);
@@ -31,31 +61,32 @@ const db = getDatabase(app);
 --------------------------------*/
 function buildInventoryGrid() {
   const grid = document.getElementById("inventory-grid");
+  grid.innerHTML = "";
 
-  for (let i = 0; i < ITEM_COUNT; i++) {
+  inventoryItems.forEach((item, index) => {
     const slot = document.createElement("div");
     slot.className = "inventory-slot";
 
     const container = document.createElement("div");
     container.className = "item-container";
 
-    const item = document.createElement("img");
-    item.src = `inv_${i + 1}.png`;
-    item.className = "inventory-item";
-    item.dataset.index = i;
-    item.draggable = false;
+    const inventoryImg = document.createElement("img");
+    inventoryImg.src = item.image;
+    inventoryImg.className = "inventory-item";
+    inventoryImg.dataset.index = index;
+    inventoryImg.title = item.title;
+    inventoryImg.alt = item.title;
 
-    const overlay = document.createElement("img");
-    overlay.src = "overlay.png";
-    overlay.className = "overlay-item";
-    overlay.dataset.index = i;
-    overlay.draggable = false;
+    const overlayImg = document.createElement("img");
+    overlayImg.src = "overlay.png";
+    overlayImg.className = "overlay-item";
+    overlayImg.dataset.index = index;
 
-    container.appendChild(item);
-    container.appendChild(overlay);
+    container.appendChild(inventoryImg);
+    container.appendChild(overlayImg);
     slot.appendChild(container);
     grid.appendChild(slot);
-  }
+  });
 }
 
 /* -----------------------------
